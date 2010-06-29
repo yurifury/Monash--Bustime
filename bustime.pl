42f44edd0e51a5fb28fa8e2c4b7e18743e32a2cf
Yuri Feldman <yuri@tbqh.net>
2010-06-30 07:54:43 +1000

    better control structure

--- bustime.pl
+++ bustime.pl
@@ -6,8 +6,8 @@ use strict;
 my $toUni = $ARGV[0] eq "from" ? 0 : 1;
 my $direction = $toUni ? "to" : "from";
 
-#my $theTime = `date +'%H%M'`;
-my $theTime = "2321";
+my $theTime = `date +'%H%M'`;
+#my $theTime = "2321";
 my @shuttlebus = qw(0730 0755 0820 0845 0910 0935 1000 1025 1050 1115 1140 1205 1230 1255 1320 1345 1410 1435 1500 1525 1550 1615 1640 1705 1730 1755 1820 1845 1910 1935 2000 2025 2050 2115);
 
 if ($toUni) {
@@ -17,7 +17,7 @@ if ($toUni) {
 }
 
 my $i = 0;
-until ($theTime < $shuttlebus[$i] or $i >= $#shuttlebus) {
+while ($i < @shuttlebus && $shuttlebus[$i] < $theTime) {
     $i++;
 }
 if ($i < $#shuttlebus) {
