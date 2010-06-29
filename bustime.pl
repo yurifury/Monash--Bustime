8415f02716a3ef78f523bef83c297b9a08ddf05c
Yuri Feldman <yuri@tbqh.net>
2010-06-29 23:46:21 +1000

    drunken coding

--- bustime.pl
+++ bustime.pl
@@ -1,15 +1,27 @@
 #!/usr/bin/perl
 
+use warnings;
 use strict;
-=dur
 
-if ($#ARGV != 1) {
-    print "one argument: 'to' or 'from' Monash Clayton\n";
-    exit;
-}
-=cut
+my $toUni = $ARGV[0] eq "from" ? 0 : 1;
+my $direction = $toUni ? "to" : "from";
 
-my $theTime = system('date +"%H%M"');
-print $theTime;
+#my $theTime = `date +'%H%M'`;
+my $theTime = "2321";
 my @shuttlebus = qw(0730 0755 0820 0845 0910 0935 1000 1025 1050 1115 1140 1205 1230 1255 1320 1345 1410 1435 1500 1525 1550 1615 1640 1705 1730 1755 1820 1845 1910 1935 2000 2025 2050 2115);
 
+if ($toUni) {
+    push(@shuttlebus, "2215");
+} else {
+    push(@shuttlebus, "2145");
+}
+
+my $i = 0;
+until ($theTime < $shuttlebus[$i] or $i >= $#shuttlebus) {
+    $i++;
+}
+if ($i < $#shuttlebus) {
+    print "The next three shuttle buses $direction Monash University Clayton are:\n$shuttlebus[$i]\n$shuttlebus[$i+1]\n$shuttlebus[$i+2]\n";
+} else {
+    print "Abandon all hope!\n";
+}
