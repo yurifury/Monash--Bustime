edb5daac3374840ed5a0bb6e3bf1d462354994ca
Yuri Feldman <yuri@tbqh.net>
2010-09-15 19:14:04 +1000

    better output, eta added

--- bustime.pl
+++ bustime.pl
@@ -5,6 +5,7 @@ use warnings;
 use strict;
 
 my $theTime = `date +'%H%M'`;
+chomp $theTime;
 my @shuttlebus = qw(
     0730 0755 0820 0845 0910 0935 1000 1025 1050 1115 1140 1205 1230 1255 1320
     1345 1410 1435 1500 1525 1550 1615 1640 1705 1730 1755 1820 1845 1910 1935
@@ -12,10 +13,11 @@ my @shuttlebus = qw(
 );
 
 my $i = 0;
-print "The next 5 buses are:\n";
+print "It is now $theTime.\n";
 foreach my $bustime (@shuttlebus) {
     if ($bustime > $theTime) {
-        print $bustime, "\n";
+        my $eta = $bustime - $theTime;
+        print "$bustime in $eta minutes\n";
         $i++;
     }
     last if $i == 5;
